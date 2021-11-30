511e1d4d61d2f705a47aa5a2f4b8192fcd2628a4
adam <adame.barhdadi@student.vinci.be>
2021-11-30 16:59:22 +0100

    test <1 should return 0000 (testMinuteSimple)

--- BerlinClock.php
+++ BerlinClock.php
@@ -1,1 +1,35 @@
 <?php
+class BerlinClock
+{
+//Step1 minuteSimple
+    public function testMinute2emeLigne($minute){
+        if(strlen($minute)>1){
+            $minute=substr($minute,1);
+        }
+        if($minute >= 5){
+            $minute=$minute-5;
+        }
+
+        if($minute==0) {
+            return "0000";
+        }
+        else if($minute==1) {
+            return "Y000";
+        }
+        else if ($minute==2) {
+            return "YY00";
+        }
+        else if ($minute==3) {
+            return "YYY0";
+        }
+        else {
+            return "YYYY";
+        }
+    }
+
+
+
+
+}
+
+?>

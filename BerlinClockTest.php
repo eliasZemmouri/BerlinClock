25e9dd3359b7f28390615feedb36c2676a38717f
adam <adame.barhdadi@student.vinci.be>
2021-11-30 17:00:17 +0100

    test 1 should return Y000 (testMinuteSimple)

---
+++ BerlinClockTest.php
@@ -0,0 +1,30 @@
+<?php
+
+
+use PHPUnit\Framework\TestCase;
+require 'BerlinClock.php';
+class BerlinClockTest extends TestCase
+{
+    public function testMinuteSimplePlusPetitQue1ShouldReturn0000(){
+
+        $berlinClock = new BerlinClock();
+
+        $actual = $berlinClock ->testMinute2emeLigne(0);
+
+        $this->assertEquals("0000",$actual);
+
+
+    }
+
+    public function testMinuteSimple1ShouldReturnY000(){
+
+        $berlinClock = new BerlinClock();
+
+        $actual = $berlinClock ->testMinute2emeLigne(1);
+
+        $this->assertEquals("Y000",$actual);
+
+
+    }
+
+}
